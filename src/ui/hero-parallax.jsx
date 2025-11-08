c33507f46adacb63078e76c013963659e1482ff5
Richita Patnaik <richierich-04@users.noreply.github.com>
2025-11-08 23:03:50 +0530

    update

--- src/ui/hero-parallax.jsx
+++ src/ui/hero-parallax.jsx
@@ -5,6 +5,8 @@ import {
   useTransform,
   useSpring,
 } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
 
 export const HeroParallax = ({ products }) => {
   const firstRow = products.slice(0, 5);
@@ -116,24 +118,22 @@ export const ProductCard = ({ product, translate }) => {
       key={product.title}
       className="group/product h-96 w-[30rem] relative flex-shrink-0"
     >
-      <a
+      <Link
         href={product.link}
         className="block group-hover/product:shadow-2xl"
-        target="_blank"
-        rel="noopener noreferrer"
       >
-        <img
+        <Image
           src={product.thumbnail}
-          height="600"
-          width="600"
-          className="object-cover object-left-top absolute h-full w-full inset-0 rounded-lg"
+          height={600}
+          width={600}
+          className="object-cover object-left-top absolute h-full w-full inset-0"
           alt={product.title}
         />
-      </a>
-      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none rounded-lg"></div>
-      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white font-semibold text-xl">
+      </Link>
+      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
+      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
       </h2>
     </motion.div>
   );
-}
+};
